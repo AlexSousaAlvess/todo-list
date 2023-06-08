13d23d541472f6d7796e0621de323e7f9cbe5b10
Alex Sousa <alexsousaalves317@gmail.com>
2023-06-08 16:43:37 -0300

    create model, repository and fihish service and service implemented

--- api/src/main/java/com/todolist/api/models/TodoModel.java
+++ api/src/main/java/com/todolist/api/models/TodoModel.java
@@ -1,7 +1,18 @@
 package com.todolist.api.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.UUID;
+
 public class TodoModel{
-    private String
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column
+    private Boolean status;
 }
